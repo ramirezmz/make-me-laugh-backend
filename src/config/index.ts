8135aaddd46420119e4e4a0efd29359efedb6cc3
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-20 21:18:47 -0300

    feat: add firebase to the project

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appiID: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
+
+import { firebaseConfig } from './config/index'
+import { initializeApp } from "firebase/app"
+import { getAuth} from "firebase/auth"
 import 'dotenv/config'
 import express from 'express'
-
+import cors from 'cors'
 
 const app = express()
+
+const firebase = initializeApp(firebaseConfig)
+const auth = getAuth()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.json({ type: "application/vnd.api+json" }))
 
 app.get('/', (req, res) => {
   res.send('Hello world')
